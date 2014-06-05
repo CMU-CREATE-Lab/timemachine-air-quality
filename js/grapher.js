7b2f0fd66b04315c19b3112ca7ca555777d55072
Yen-Chia Hsu <legenddolphin@gmail.com>
2014-06-05 16:58:50 -0400

    Tweaked the layout.

--- js/grapher.js
+++ js/grapher.js
@@ -135,7 +135,7 @@ function seekTimeMachineToCurrentCursorPosition(event) {
 var createCharts = function() {
   $("#grapher").append('<div id="dateAxisContainer"><div id="dateAxis"></div></div>');
 
-  var minDate_millisecs = currentDate_millisecs - 864000000;
+  var minDate_millisecs = currentDate_millisecs - 1209600000;
 
   dateAxis = new DateAxis("dateAxis", "horizontal", {
     min: minDate_millisecs / 1000,
